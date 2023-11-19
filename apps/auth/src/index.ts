9bfa2ef5e20b444059a14bbbc0ef74151bbfe36a
Artem Zinoviev <a.zinoviev@sdventures.com>
2023-11-19 11:40:51 +0300

    Upgraded packages for Node20 support

--- apps/auth/src/index.ts
+++ apps/auth/src/index.ts
@@ -9,6 +9,6 @@ fastify.listen({ port: env.PORT, host: '0.0.0.0' }, (err, address) => {
     process.exit(1);
   }
 
-  fastify.log.info(env, '[SERVER] ENV');
-  fastify.log.info(`[SERVER] API ${address.replace('0.0.0.0', 'localhost')}`);
+  fastify.log.info(env, '[SERVER] ENV =>');
+  fastify.log.info(`[SERVER] API => ${address.replace('0.0.0.0', 'localhost')}`);
 });
