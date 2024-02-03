7f9ad6b24f358d425aba1c323a1c67fef07a5e9e
Artem Zinoviev <7binary@gmail.com>
2024-02-04 01:23:47 +0300

    Updated npm-module versions to latest fastify/prisma

--- apps/tickets/src/routes/__test__/tickets-list.route.test.ts
+++ apps/tickets/src/routes/__test__/tickets-list.route.test.ts
@@ -6,12 +6,27 @@ test('Tickets List', async (t) => {
   t.teardown(() => app.close());
   await app.ready();
 
+  const { accessToken } = app.auth.generateAuthTokens({ id: 1 });
+  await app.inject({
+    method: 'POST',
+    path: '/api/tickets',
+    body: { title: 'First ticket', price: 1 },
+    headers: { Authorization: `Bearer ${accessToken}` },
+  });
+  await app.inject({
+    method: 'POST',
+    path: '/api/tickets',
+    body: { title: 'Second ticket', price: 2 },
+    headers: { Authorization: `Bearer ${accessToken}` },
+  });
+
   const ticketsSuccess = await app.inject({
     method: 'GET',
     path: '/api/tickets',
   });
   t.equal(ticketsSuccess.statusCode, 200, 'returs a 200 on success');
-  t.equal(Array.isArray(ticketsSuccess.json()), true, 'is array');
+  const tickets = ticketsSuccess.json();
+  t.equal(Array.isArray(tickets) && tickets.length >= 2, true, 'tickets is array of 2 records');
 
   t.end();
 });
